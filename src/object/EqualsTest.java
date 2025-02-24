289af04c26f087144d16b6940cad383438f91e25
goforit615 <bupark82@gsm.hs.kr>
2025-02-24 17:12:06 +0900

    initial commit

---
+++ src/object/EqualsTest.java
@@ -0,0 +1,41 @@
+package object;
+
+class Student {
+    int studentId;
+    String studentName;
+
+    public Student(int studentId, String studentName) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+    }
+
+    public String toString() {
+        return studentId + "," + studentName;
+    }
+}
+public class EqualsTest {
+    public static void main(String[] args) {
+        Student studentLee = new Student(100, "이상원");
+        Student studentLee2 = studentLee;
+        Student studentSang = new Student(100, "이상원");
+        // 동일한 주소의 두 인스턴스 비교
+        if(studentLee == studentLee2)
+            System.out.println("studentLee와 studentLee2의 주소는 같습니다.");
+        else
+            System.out.println("studentLee와 studentLee2의 주소는 다릅니다.");
+        if(studentLee.equals(studentLee2))
+            System.out.println("studentLee와 studentLee2는 동일합니다.");
+        else
+            System.out.println("studentLee와 studentLee2는 동일하지 않습니다.");
+
+        //주소는 다르지만 equals 의 결과가 true 인 경우
+        if(studentLee == studentSang)
+            System.out.println("studentLee와 studentSang의 주소는 같습니다.");
+        else
+            System.out.println("studentLee와 studentSang의 주소는 다릅니다.");
+        if(studentLee.equals(studentSang))
+            System.out.println("studentLee와 studentSang은 동일합니다.");
+        else
+            System.out.println("studentLee와 studentSang은 동일하지 않습니다.");
+    }
+}
